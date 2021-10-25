e0987a8501bc1d1a2805f62f392b83963f39ae46
nilton <nfescame@gmail.comq:qclear>
2021-10-25 00:27:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import api from "./api";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [catState, setCatState] = useState([]);
+
+  const update = () => {
+    api
+      .get("")
+      .then((response) => {
+        setCatState(response.data[0].url);
+      })
+      .catch((err) => {
+        console.log("error");
+      });
+  };
+
+  useEffect(() => {
+    update();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='box'>
+        <div className='card'>
+          <div className='card-body'>
+            <h5 className='card-title'>Cat Random</h5>
+            <p className='card-text'>
+              Click on the button and see a cute kitten.
+            </p>
+            <button onClick={update} type='button' class='btn btn-info'>
+              Random
+            </button>
+          </div>
+        </div>
+        <img src={catState} className='card-img-top' alt='...' />
+      </div>
     </div>
   );
 }
-
-export default App;
